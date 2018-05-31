c4b4fcd843d155c93d041c712168a138da6ee29b
santi.jarusombat <santi.jarusombat@gmail.com>
2018-05-31 14:47:46 +0700

    first version

---
+++ downloadTool.js
@@ -0,0 +1,144 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const minimist = require('minimist')
+const electron = require('electron')
+const fse = require("fs-extra")
+const path = require("path");
+const downloader_1 = require("./downloader");
+
+// Module to control application life.
+const app = electron.app
+const session = electron.session
+const network = electron.network
+const ipcMain = electron.ipcMain;
+
+var url = '';
+var throttle = 0;
+
+let mainWindow
+let self = this;
+
+let _req
+
+function stopDownload() {
+    if (_req) _req.request.abort('User canceled');
+}
+
+function clearDownload(url2) {
+    stopDownload();
+    var dest = path.join(process.cwd(), url2.split('/').pop());
+
+    if (fse.existsSync(dest)) {
+        fse.unlinkSync(dest)
+    }
+}
+
+function download() {
+
+    writeProgress("download ()")
+    let self = this;
+    return new Promise((resolve, reject) => {
+        var dest = path.join(process.cwd(), url.split('/').pop());
+        writeProgress('url: ' + dest);
+        writeProgress('dest: ' + dest);
+        writeProgress('throttle: ' + throttle);
+        
+        let option = {
+            resume: true,
+            output: {
+                path: path.join(dest),
+                filename: ''
+            },
+        };        
+               
+        let downloader = new downloader_1.Downloader();
+        let unlinkAndReject = ((msg) => () => {
+            fse.unlink(dest);
+            reject(msg);
+        });
+        downloader.download(url, option, throttle).then(req => {
+            _req = req;
+            let stream = req.stream;
+            let progress = true;
+
+            req.request.on('error', unlinkAndReject('HTTP request error'));
+            req.request.on('abort', () => {
+                stream.close();
+            });
+
+            req.request.on('response', (response) => {
+                if (response.statusCode !== 200 && response.statusCode !== 206) {
+                    return reject(`Error response from server: ${response.statusCode.toString()}`);
+                }
+                response.pipe(stream);
+                response.on('end', () => {
+                    done();
+                });
+                let streamDone = false;
+                stream.on('close', () => {
+                    streamDone = true;
+                    return resolve(true);
+                });
+                setTimeout(() => {
+                    if (!streamDone) {
+                        // writeProgress('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  [Deployment] Reset Bandwidth');
+                        // req.session.disableNetworkEmulation();
+                        // req.session.enableNetworkEmulation({
+                        //     downloadThroughput: parseInt(throttle),
+                        // });
+                        writeProgress('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  [Deployment] Download Timeout');
+                        req.request.abort();
+                        // stream.close();
+                        resolve(false);
+                    }
+                }, 10 * 60 * 1000); // 10 minutes
+            });
+            
+            req.request.on('progress', function (state) {
+                
+                    let date = new Date();
+                    writeProgress(date);
+                    writeProgress('progress:' + JSON.stringify(state));
+                
+            });
+            req.request.on('close', () => {
+                progress = false;
+            });
+            stream.on('error', unlinkAndReject('Could not save file'));
+            req.request.end();
+        }).catch(e => {
+            if (e.message === 'file is done') {
+                writeProgress('[Deployment] Download is already completed');
+                done();
+                resolve(true);
+            }
+            else {
+                writeProgress(e);
+                unlinkAndReject(e);
+            }
+        });
+    });
+}
+
+function writeProgress(text) {
+    // console.log(text);
+    self.mainWindow.webContents.send('progress', text);
+}
+
+function done() {
+    writeProgress('[Deployment] Download completed');
+    self.mainWindow.webContents.send('done');
+}
+
+module.exports = {
+    startDownloadWithDelay: function(minutes, url2, bandwidth2, mainWindow) {
+        url = url2;
+        throttle = bandwidth2;
+        self.mainWindow = mainWindow;
+        let date = new Date();
+        writeProgress(date);
+        writeProgress(`Wait minutes: ${minutes}`);
+        myVar = setTimeout(download, minutes * 60 * 1000); //milliseconds.
+    }, stopDownload, clearDownload
+};
---
+++ downloader.js
@@ -0,0 +1,83 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const progress = require('request-progress');
+const R = require('ramda');
+class Downloader {
+    download(downloadurl, option, throttle = 0) {
+        let link = url.parse(downloadurl);
+        option = R.merge({
+            url: downloadurl,
+            resume: true,
+            method: 'GET',
+            headers: {},
+            output: {
+                path: '/tmp',
+                filename: link.pathname.split('/').pop()
+            }
+        }, option);
+        const outputfilename = path.resolve(R.path(['output', 'path'], option), R.path(['output', 'filename'], option));
+        console.log('outputfilename: '+ outputfilename);
+        const supportRanges = new Promise(function (resolve, reject) {
+            electron_1.net.request({
+                method: 'HEAD',
+                host: link.hostname,
+                path: link.path,
+                port: Number(link.port),
+            }, function (req) {
+                resolve({
+                    resume: 'accept-ranges' in req.headers,
+                    contentLength: req.headers['content-length'],
+                });
+            })
+                .on('aborted', () => { reject('The request has been aborted by the server.'); })
+                .end();
+        });
+        const blockSize = new Promise(function (resolve, reject) {
+            fs.exists(outputfilename, function (exists) {
+                if (exists) {
+                    fs.stat(outputfilename, function (err, stat) {
+                        resolve(stat.size);
+                    });
+                }
+                else {
+                    resolve(0);
+                }
+            });
+        });
+        return Promise.all([supportRanges, blockSize])
+            .then(function (answer) {
+            const resume = answer[0]['resume'];
+            const contentLength = answer[0]['contentLength'];
+            const bytes = answer[1];
+            if (contentLength <= bytes)
+                throw new Error('file is done');
+            option.resume = option.resume && resume;
+            var ses = electron_1.session.fromPartition('downloader', { cache: false });
+            console.log('Downloader.download => throttle: ' + throttle);
+            ses.enableNetworkEmulation({
+                downloadThroughput: parseInt(throttle),
+            });
+            option.session = ses;
+            
+            if (option.resume) {
+                option.headers['Range'] = `bytes=${bytes}-${contentLength}`;
+            }
+
+
+            return option;
+        }).then(function (option) {
+            return {
+                request: progress(electron_1.net.request(option)),
+                stream: fs.createWriteStream(outputfilename, { flags: option.resume ? 'a' : 'w', autoClose: true }),
+                session: option.session
+            };
+        });
+    }
+}
+exports.Downloader = Downloader;
+
+//# sourceMappingURL=downloader.js.map
